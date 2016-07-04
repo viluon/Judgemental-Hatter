29c1d3439f48d7d52b888c1e600fbd667667e718
Andrew Kvapil <viluon@espiv.net>
2016-07-04 15:41:03 +0200

    Improve Design; Add Categories and Descriptions

--- main.lua
+++ main.lua
@@ -12,6 +12,9 @@ local scroll = 0
 local	draw
 
 local item_not_judged_text = "Category not judged"
+local arrow_position = 5
+
+local is_game = false
 
 local categories = {
 	{
@@ -19,24 +22,136 @@ local categories = {
 		range = { 0, 3 };
 		name = "Creativity";
 		descriptions = {
-			"poop";
+			[[
+- This type of projects is
+  very well known
+- Project takes too much
+  inspiration from
+  others of its kind]];
+			[[
+- This type of projects is
+  very well known
++ Author included a few
+  original ideas]];
+			[[
+* This type of projects is
+  either uncommon or
+  doesn't take much inspiration
+  from others
++ Author included a number of
+  original ideas or resolved
+  a major issue present
+  in other projects of this
+  kind
+]];
+			[[
++ This project is unlike
+  any other
++ A number of great ideas
+  and innovations are
+  present
+]];
+		};
+		labels = {
+			"Unoriginal";
+			"Common";
+			"Original";
+			"Outstanding";
+		}
+	};
+	{
+		value = false;
+		range = { 0, 10 };
+		name = "Usefulness";
+		descriptions = {
+			[[
+- Project has no real use
+- 
+]];
 			"less poop";
 			"almost no poop";
 			"only golden poop";
 		};
+		labels = {
+			"Useless";
+			"Useless";
+			"Useless";
+			"Occasionally Helpful";
+			"Occasionally Helpful";
+			"Helpful";
+			"Very Helpful";
+			"Handy";
+			"For Every Day Usage";
+			"Exceptionally Useful";
+		}
 	};
 	{
 		value = false;
-		range = { 0, 3 };
-		name = "Poopitivity";
+		range = { 0, 6 };
+		name = "Design";
 		descriptions = {
 			"poop";
 			"less poop";
 			"almost no poop";
 			"only golden poop";
 		};
+		labels = {
+			"Unoriginal";
+			"Common";
+			"Original";
+			"Outstanding";
+		}
+	};
+	{
+		value = false;
+		range = { 0, 4 };
+		name = "Speed";
+		descriptions = {
+			[[
+- 
+]];
+			"less poop";
+			"almost no poop";
+			"only golden poop";
+		};
+		labels = {
+			"Very Slow";
+			"Responsive";
+			"Quick";
+			"Swift";
+		}
+	};
+	{
+		value = false;
+		range = { 0, 10 };
+		name = "Functionality";
+		descriptions = {
+			"poop";
+			"less poop";
+			"almost no poop";
+			"only golden poop";
+		};
+		labels = {
+			"Unusable";
+			"Broken";
+			"Broken";
+			"Broken";
+		}
 	};
 }
+
+if is_game then
+	categories[ 2 ] = {
+		value = false;
+		range = { 0, 10 };
+		name = "Fun";
+
+		descriptions = {};
+		labels = {
+			"Boring";
+		};
+	}
+end
 
 --- Redraw the screen
 -- @return nil
@@ -53,28 +168,44 @@ function draw()
 
 		term.setCursorPos( 2, scroll + total_y )
 		term.setTextColour( cat_item.colour or colours.black )
-		term.write( cat_item.name )
+		term.write( cat_item.name .. ": " .. ( cat_item.value and cat_item.labels[ cat_item.value + 1 ] or "Unknown" ) )
 
 		term.setTextColour( colours.grey )
-		term.setCursorPos( 5, scroll + total_y + 1 )
-		term.write( "^" )
-		term.setCursorPos( 5, scroll + total_y + 3 )
-		term.write( "v" )
+		term.setCursorPos( arrow_position, scroll + total_y + 1 )
+		term.write( "\30" )
+		term.setCursorPos( arrow_position, scroll + total_y + 3 )
+		term.write( "\31" )
 
 		term.setTextColour( colours.black )
-		term.setCursorPos( 5, scroll + total_y + 2 )
+		term.setCursorPos( arrow_position, scroll + total_y + 2 )
 		term.write( cat_item.value or "-" )
 
 		term.setTextColour( colours.grey )
 
+		local description = ( cat_item.value and cat_item.descriptions[ cat_item.value + 1 ] or item_not_judged_text ) .. "\n"
 		local c = 1
-		for line in ( ( cat_item.value and cat_item.descriptions[ cat_item.value + 1 ] or item_not_judged_text ) .. "\n" ):gmatch( ".+\n" ) do
+
+		for line in description:gmatch( "[^\n]+\n" ) do
 			term.setCursorPos( w / 5, scroll + total_y + c )
+
+			if line:sub( 1, 1 ) == "+" then
+				term.setTextColour( colours.green )
+
+			elseif line:sub( 1, 1 ) == "-" then
+				term.setTextColour( colours.red )
+
+			elseif line:sub( 1, 1 ) == "*" then
+				term.setTextColour( colours.grey )
+
+			end
+
 			term.write( line )
 			c = c + 1
 		end
 
-		total_y = total_y + 4
+		local _, newline_count = description:gsub( "\n", "" )
+
+		total_y = total_y + math.max( 4, newline_count + 2 )
 	end
 
 	term.setCursorPos( 2, 1 )
@@ -101,7 +232,7 @@ while running do
 		for i = #categories, 1, -1 do
 			cat_item = categories[ i ]
 
-			if cat_item.y_pos and cat_item.y_pos - ev[ 4 ] + scroll <= 0 then
+			if cat_item.y_pos and cat_item.y_pos - ev[ 4 ] + scroll <= 0 and ev[ 3 ] == arrow_position then
 				local relative_position = cat_item.y_pos - ev[ 4 ] + scroll
 
 				if relative_position == -1 then
